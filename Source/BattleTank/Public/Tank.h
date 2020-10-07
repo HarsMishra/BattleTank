44b5166fd316e6876ccaebdc6f8671d389bfe938
LAPTOP-3HAD935J\hp <harsmishra01@gmail.com>
2020-10-07 18:13:17 +0530

    :)

---
+++ Source/BattleTank/Public/Tank.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h"  //Put new includes above
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTankDelegate);
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+
+public:
+	// call by the engine when actor damage is dealt
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	// Return current health as a percenatage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FTankDelegate OnDeath;
+
+private:
+	// Sets default values for this pawn's properties
+	ATank();
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int32 StartingHealth = 100;
+
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	int32 CurrentHealth; // Initialised in Begin Play
+};
---
+++ Source/BattleTank/Public/TankBarrel.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS( meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public: 
+	// -1 is max downward movement, and +1 is max up movement
+	void Elevate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MaxDegreesPerSecond = 10;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MaxElevationDegrees = 40;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MinElevationDegrees = 0;
+	
+};
